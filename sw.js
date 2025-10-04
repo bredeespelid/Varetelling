405667ea10999b5c508905e530248092d7c0b732
Brede <39888226+bredeespelid@users.noreply.github.com>
2025-10-04 23:12:06 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'gb-varetelling-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => self.clients.claim());
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const cp = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp)).catch(()=>{});
+      return resp;
+    }).catch(() => r))
+  );
+});
